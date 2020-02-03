6fdc5902ad99add50a04677edfff204c5571ee28
P2402510 <P2402510@my365.dmu.ac.uk>
2020-02-03 16:51:17 +0000

    Added testing and class for stock management.

---
+++ SkyGamesTesting/tstStock.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Sky_Games_Testing
+{
+    [TestClass]
+    public class tstStock
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            clsStock aStock = new clsStock();
+            Assert.IsNotNull(aStock);
+        }
+    }
+}
