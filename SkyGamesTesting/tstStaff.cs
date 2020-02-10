cf2f3f06b12cd7c717e116095591c4deac0b1810
p1715322x <p1715322x@GWHXX284WIN007W>
2020-02-10 16:39:54 +0000

    Added testing and class for staff management

---
+++ SkyGamesTesting/tstStaff.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using ClassLibrary1;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Sky_Games_Testing
+{
+    [TestClass]
+    public class tstStaff
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            clsStaff AStaff = new clsStaff();
+            Assert.IsNotNull(AStaff);
+        }
+    }
+}
